793415365792071641f9bdac3f285d4005572fac
ESB19600 <>
2019-12-02 17:37:42 +0800

    add springboot-vue and vuetest

---
+++ vuetest/src/components/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="User Account:"
+        label-for="input-1"
+        description="Do insert with cautious."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.userAccout"
+          type="text"
+          required
+          placeholder="Enter user account"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+        <b-form-input id="input-2" v-model="form.userPwd" type="password" required placeholder="Enter Password"></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        userAccout: '',
+        userPwd: ''
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset (event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.userAccout = ''
+      this.form.userPwd = ''
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
